0e1d90906add4c781dbb654c6091f7b951db2ef1
Similisi <73776571+Similisi@users.noreply.github.com>
2022-12-07 10:53:58 +0100

    Update Enigma.h

--- code/Enigma.h
+++ code/Enigma.h
@@ -1,10 +1,11 @@
 #include "Encrypt.h" 
 class Enigma : public Encrypt {
 	public:
-		Enigma(std::string _cle,std::string messagedecr);
+		Enigma(std::string _cle,std::string messagedecr,std::string messagecr);
 		void encode() override;
 		void decode() override;
 		std::string makecle()
 	private:
 		std::string _cle;
+		std::string _alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 };
