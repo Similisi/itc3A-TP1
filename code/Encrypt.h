010bd0934b9bba9839cc86c5bdf1ec4906f51d2b
Similisi <73776571+Similisi@users.noreply.github.com>
2022-12-07 10:52:33 +0100

    Update Encrypt.h

--- code/Encrypt.h
+++ code/Encrypt.h
@@ -7,7 +7,7 @@ class Encrypt {
 		std::string _plain;
 		std::string _cipher;
 	public:
-		Encrypt(std::string messagedecr);
+		Encrypt(std::string message,std::string messagecrypte);
 		virtual void encode() = 0;
 		virtual void decode() = 0;
 		std::string plain();
