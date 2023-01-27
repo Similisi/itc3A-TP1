52f359ceae95302e1f64b366127cae36d2e239bb
Similisi <johannfouchard25@gmail.com>
2023-01-27 15:13:23 +0100

    ajout des commentaires

--- code/Encrypt.cpp
+++ code/Encrypt.cpp
@@ -1,17 +1,19 @@
 #include "Encrypt.h" 
 Encrypt::Encrypt(std::string messagedecr,std::string messagecrypte): _plain(messagedecr), _cipher(messagecrypte){}
-
+//retourne message non chiffré
 std::string Encrypt::plain(){
 	return _plain;
 }
+//retourne message chiffré
 std::string Encrypt::cipher() {
 	return _cipher;
 }
 
-
+//ajoute message non chiffré
 void Encrypt::setplain(std::string text){
 	_plain = text;
 }
+//ajoute message chiffré
 void Encrypt::setcipher(std::string text){
 	_cipher = text;
 }
--- code/Enigma.cpp
+++ code/Enigma.cpp
@@ -1,65 +1,37 @@
 #include "Enigma.h" 
 Enigma::Enigma(std::string messagedecr,std::string messagecr) : Encrypt(messagedecr,messagecr)
 {}
+//methode d'encodage
 void Enigma::encode(){
 	std::string messageDecode = "";
 	std::vector<std::string> listeRotor = _listecle;
 	std::string Alphabet= "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
-	//reverse(listeRotor.begin(),listeRotor.end());
-	//std::cout << "rotor 1 = " << listeRotor.at(0) << std::endl;
-	//std::cout << "rotor 2 = " << listeRotor.at(1) << std::endl;
 	for(char lettredansmot : plain()){
 		char lettre = lettredansmot;
 		for(std::string rotor : listeRotor){ //faire pour 2 rotor ( 2 clés)
-			//std::cout << "rotor = " << rotor << std::endl;
 			int pos = Alphabet.find(toupper(lettre));
-			//std::cout << lettre << " devient ";
 			lettre = rotor.at(pos);
-			//std::cout << lettre << std::endl;
 		}
-		//std::cout << "message = " << messageDecode << std::endl;
 		messageDecode += lettre;
 	}
 	_cipher = messageDecode;
-	//std::cout << "resultat = " << messageDecode << std::endl;
-	/*std::string Alphabet= "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
-	std::string encodage = _plain ;
-	std::string buffer = "" ;
-	int tailleMessage= _plain.size();
-	for(std::string clef: _listecle){
-		for (int i = 0 ; i < tailleMessage ; i++ ){
-			for (int j = 0 ; j < 26 ; j++){				
-				if (encodage.at(i)==Alphabet.at(j)){
-						encodage.at(i)=clef.at(j) ;
-					}
-				}	
-			}
-			buffer=encodage;
-		}
-	_cipher=buffer ;*/
 }
+//methode de décodage
 void Enigma::decode(){
 	std::string messageDecode = "";
 	std::vector<std::string> listeRotor = _listecle;
 	reverse(listeRotor.begin(),listeRotor.end());
-	//std::cout << "rotor 1 = " << listeRotor.at(0) << std::endl;
-
-	//std::cout << "rotor 2 = " << listeRotor.at(1) << std::endl;
 	for(char lettredansmot : cipher()){
 		char lettre = lettredansmot;
 		for(std::string rotor : listeRotor){ //faire pour 2 rotor ( 2 clés)
-			//std::cout << "rotor = " << rotor << std::endl;
 			int pos = rotor.find(toupper(lettre));
-			//std::cout << lettre << " devient ";
 			lettre = _alphabet.at(pos);
-			//std::cout << lettre << std::endl;
 		}
-		//std::cout << "message = " << messageDecode << std::endl;
 		messageDecode += lettre;
 	}
 	_plain = messageDecode;
-	//std::cout << "resultat = " << messageDecode << std::endl;
 }
+//méthode pour créer une clé
 std::string Enigma::makecle(){
 	std::string Alphabet= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	std::string cle;
@@ -70,11 +42,11 @@ std::string Enigma::makecle(){
 	sleep(1);
 	return cle;
 }
-
+//ajout de clé dans la liste des clé
 void Enigma::AjoutCle(std::string cle){
 	_listecle.push_back(cle);
 }
-
+//retourne la liste des clé
 std::vector<std::string> Enigma::GetListeCle(){
 	return _listecle;
 }
